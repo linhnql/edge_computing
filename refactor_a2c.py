36d5104b6a19634b9e4711a252e89dd849af0e44
quanglinh304 <linh.nq3004@gmail.com>
2023-01-08 12:24:12 +0700

    add

---
+++ refactor_a2c.py
@@ -0,0 +1,155 @@
+# from stable_baselines import PPO2
+from stable_baselines.common import set_global_seeds
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import A2C
+
+import time
+import matplotlib.pyplot as plt
+import gym
+import gym_offload_autoscale
+import numpy as np
+import pandas as pd
+import os
+
+# start_time = time.time()
+# my_path = os.path.abspath('res/')
+
+
+def set_seed(rand_seed):
+    set_global_seeds(100)
+    env.env_method('seed', rand_seed)
+    np.random.seed(rand_seed)
+    os.environ['PYTHONHASHSEED'] = str(rand_seed)
+    model.set_random_seed(rand_seed)
+
+
+rand_seed = 1234
+x = 0.5
+
+env = gym.make('offload-autoscale-v0', p_coeff=x)
+# Optional: PPO2 requires a vectorized environment to run
+# the env is now wrapped automatically when passing it to the constructor
+env = DummyVecEnv([lambda: env])
+model = A2C(MlpPolicy, env, verbose=1, seed=rand_seed)
+model.learn(total_timesteps=1000)
+
+# ppo
+rewards_list = []
+avg_rewards = []
+rewards_time_list = []
+avg_rewards_time_list = []
+rewards_bak_list = []
+avg_rewards_bak_list = []
+rewards_bat_list = []
+avg_rewards_bat_list = []
+avg_rewards_energy_list = []
+ppo_data = []
+
+train_time_slots = 20000
+t_range = 2000
+
+# PPO
+set_seed(rand_seed)
+obs = env.reset()
+for i in range(t_range):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = env.step(action)
+    rewards_list.append(1 / rewards)
+    avg_rewards.append(np.mean(rewards_list[:]))
+    t, bak, bat = env.render()
+    rewards_time_list.append(t)
+    avg_rewards_time_list.append(np.mean(rewards_time_list[:]))
+    rewards_bak_list.append(bak)
+    avg_rewards_bak_list.append(np.mean(rewards_bak_list[:]))
+    rewards_bat_list.append(bat)
+    avg_rewards_bat_list.append(np.mean(rewards_bat_list[:]))
+    avg_rewards_energy_list.append(avg_rewards_bak_list[-1]+avg_rewards_bat_list[-1])
+    ppo_data.append([avg_rewards_time_list[-1], avg_rewards_bak_list[-1], avg_rewards_bat_list[-1]])
+    if dones:
+        env.reset()
+    # env.render()
+
+
+# print('--RESULTS--')
+# print('{:15}{:30}{:10}{:10}{:10}'.format('method','total cost','time','bak','bat'))
+# print('{:15}{:<30}{:<10.5}{:<10.5}{:<10.5}'.format('PPO', avg_rewards[-1], avg_rewards_time_list[-1], avg_rewards_bak_list[-1], avg_rewards_bat_list[-1]))
+# end_time = time.time()
+# print('elapsed time:', end_time-start_time)
+
+# #total cost
+# df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards})
+# plt.xlabel("Time Slot")
+# plt.ylabel("Time Average Cost")
+# plt.plot('x', 'y_1', data=df, marker='o', markevery = int(t_range/10), color='red', linewidth=1, label="ppo")
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_total_p='+str(x)+'_.xlsx'
+# export_excel = df.to_excel (os.path.join(my_path, my_file), index = None, header=True)
+# my_file = 'p='+str(x)+'/ppo/ppo_total_p='+str(x)+'_.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
+# #time cost
+# dft=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_time_list})
+# plt.xlabel("Time Slot")
+# plt.ylabel("Time Average Delay Cost")
+# plt.plot('x', 'y_1', data=dft, marker='o', markevery = int(t_range/10), color='red', linewidth=1, label="ppo")
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_time_p='+str(x)+'_.xlsx'
+# export_excel = dft.to_excel (os.path.join(my_path, my_file), index = None, header=True)
+# my_file = 'p='+str(x)+'/ppo/ppo_time_p='+str(x)+'_.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
+# #bak-up cost
+# dfbak=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_bak_list})
+# plt.xlabel("Time Slot")
+# plt.ylabel("Time Average Back-up Power Cost")
+# plt.plot('x', 'y_1', data=dfbak, marker='o', markevery = int(t_range/10), color='red', linewidth=1, label="ppo")
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_backup_p='+str(x)+'_.xlsx'
+# export_excel = dfbak.to_excel (os.path.join(my_path, my_file), index = None, header=True)
+# my_file = 'p='+str(x)+'/ppo/ppo_backup_p='+str(x)+'_.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
+# #battery cost
+# dfbat=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_bat_list})
+# plt.xlabel("Time Slot")
+# plt.ylabel("Time Average Battery Cost")
+# plt.plot('x', 'y_1', data=dfbat, marker='o', markevery = int(t_range/10), color='red', linewidth=1, label="ppo")
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_battery_p='+str(x)+'_.xlsx'
+# export_excel = dfbat.to_excel (os.path.join(my_path, my_file), index = None, header=True)
+# my_file = 'p='+str(x)+'/ppo/ppo_battery_p='+str(x)+'_.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
+# #energy cost
+# dfe=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_energy_list})
+# plt.xlabel("Time Slot")
+# plt.ylabel("Time Average Energy Cost")
+# plt.plot('x', 'y_1', data=dfe, marker='o', markevery = int(t_range/10), color='red', linewidth=1, label="ppo")
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_energy_p='+str(x)+'_.xlsx'
+# export_excel = dfe.to_excel (os.path.join(my_path, my_file), index = None, header=True)
+# my_file = 'p='+str(x)+'/ppo/ppo_energy_p='+str(x)+'_.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
+
+# #ppo area chart
+# plt.xlabel("Time Slot")
+# plt.ylabel("Average Costs")
+# xx = range(t_range)
+# yy = [avg_rewards_time_list, avg_rewards_bak_list, avg_rewards_bat_list]
+# fig = plt.stackplot(xx, yy, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+# hatches = ['...', '+++++', '///']
+# for s, h in zip(fig, hatches):
+#     s.set_hatch(h)
+# plt.title('PPO')
+# plt.legend()
+# plt.grid()
+# my_file = 'p='+str(x)+'/ppo/ppo_area'+'p='+str(x)+'.png'
+# plt.savefig(os.path.join(my_path, my_file))
+# plt.show()
--- refactor_result.py
+++ refactor_result.py
@@ -1,10 +1,12 @@
 import gym_offload_autoscale
 
-import refactor_ppo as ppo
-import refactor_random as random
-import refactor_myopic as myopic
-import refactor_fixed_04 as fixed_04
-import refactor_fixed_1 as fixed_1
+import refactor_ppo1_error as ppo1
+# import refactor_ppo2 as ppo2
+# import refactor_random as random
+# import refactor_myopic as myopic
+# import refactor_fixed_04 as fixed_04
+# import refactor_fixed_1 as fixed_1
+# import refactor_a2c as a2c
 # import refactor_dqn as dqn
 
 from refactor_func import *
@@ -12,49 +14,57 @@ from refactor_func import *
 
 print('--RESULTS--')
 print('{:15}{:30}{:10}{:10}{:10}'.format('method', 'total cost', 'time', 'bak', 'bat'))
-result('PPO', ppo.avg_rewards, ppo.avg_rewards_time_list,
-       ppo.avg_rewards_bak_list, ppo.avg_rewards_bat_list)
-result('Random', random.avg_rewards, random.avg_rewards_time_list,
-       random.avg_rewards_bak_list, random.avg_rewards_bat_list)
-result('Myopic', myopic.avg_rewards, myopic.avg_rewards_time_list,
-       myopic.avg_rewards_bak_list, myopic.avg_rewards_bat_list)
-result('Fixed 0.4kW', fixed_04.avg_rewards, fixed_04.avg_rewards_time_list,
-       fixed_04.avg_rewards_bak_list, fixed_04.avg_rewards_bat_list)
-result('Fixed 1kW', fixed_1.avg_rewards, fixed_1.avg_rewards_time_list,
-       fixed_1.avg_rewards_bak_list, fixed_1.avg_rewards_bat_list)
+result('PPO1', ppo1.avg_rewards, ppo1.avg_rewards_time_list,
+       ppo1.avg_rewards_bak_list, ppo1.avg_rewards_bat_list)
+# result('PPO2', ppo2.avg_rewards, ppo2.avg_rewards_time_list,
+#        ppo2.avg_rewards_bak_list, ppo2.avg_rewards_bat_list)
+# result('Random', random.avg_rewards, random.avg_rewards_time_list,
+#        random.avg_rewards_bak_list, random.avg_rewards_bat_list)
+# result('Myopic', myopic.avg_rewards, myopic.avg_rewards_time_list,
+#        myopic.avg_rewards_bak_list, myopic.avg_rewards_bat_list)
+# result('A2C', a2c.avg_rewards, a2c.avg_rewards_time_list,
+#        a2c.avg_rewards_bak_list, a2c.avg_rewards_bat_list)
+# result('Fixed 0.4kW', fixed_04.avg_rewards, fixed_04.avg_rewards_time_list,
+#        fixed_04.avg_rewards_bak_list, fixed_04.avg_rewards_bat_list)
+# result('Fixed 1kW', fixed_1.avg_rewards, fixed_1.avg_rewards_time_list,
+#        fixed_1.avg_rewards_bak_list, fixed_1.avg_rewards_bat_list)
 # result('DQN', alg_dqn.avg_rewards, alg_dqn.avg_rewards_time_list,
 #        alg_dqn.avg_rewards_bak_list, alg_dqn.avg_rewards_bat_list)
 
 
-cost("avg_total", "Time Average Cost", ppo.avg_rewards, random.avg_rewards,
-     myopic.avg_rewards, fixed_04.avg_rewards, fixed_1.avg_rewards)  # , alg_dqn.avg_rewards)
+# cost("avg_total", "Time Average Cost", ppo2.avg_rewards, random.avg_rewards,
+#      myopic.avg_rewards, fixed_04.avg_rewards, fixed_1.avg_rewards)  # , alg_dqn.avg_rewards)
 
-cost("avg_time", "Time Average Delay Cost", ppo.avg_rewards_time_list,
-     random.avg_rewards_time_list, myopic.avg_rewards_time_list,
-     fixed_04.avg_rewards_time_list, fixed_1.avg_rewards_time_list)  # , alg_dqn.avg_rewards_time_list)
+# cost("avg_time", "Time Average Delay Cost", ppo2.avg_rewards_time_list,
+#      random.avg_rewards_time_list, myopic.avg_rewards_time_list,
+#      fixed_04.avg_rewards_time_list, fixed_1.avg_rewards_time_list)  # , alg_dqn.avg_rewards_time_list)
 
-cost("avg_backup", "Time Average Back-up Power Cost", ppo.avg_rewards_bak_list,
-     random.avg_rewards_bak_list, myopic.avg_rewards_bak_list,
-     fixed_04.avg_rewards_bak_list, fixed_1.avg_rewards_bak_list)  # , alg_dqn.avg_rewards_bak_list)
+# cost("avg_backup", "Time Average Back-up Power Cost", ppo2.avg_rewards_bak_list,
+#      random.avg_rewards_bak_list, myopic.avg_rewards_bak_list,
+#      fixed_04.avg_rewards_bak_list, fixed_1.avg_rewards_bak_list)  # , alg_dqn.avg_rewards_bak_list)
 
-cost("avg_battery", "Time Average Battery Cost", ppo.avg_rewards_bat_list,
-     random.avg_rewards_bat_list, myopic.avg_rewards_bat_list, fixed_04.avg_rewards_bat_list,
-     fixed_1.avg_rewards_bat_list)  # , alg_dqn.avg_rewards_bat_list)
+# cost("avg_battery", "Time Average Battery Cost", ppo2.avg_rewards_bat_list,
+#      random.avg_rewards_bat_list, myopic.avg_rewards_bat_list, fixed_04.avg_rewards_bat_list,
+#      fixed_1.avg_rewards_bat_list)  # , alg_dqn.avg_rewards_bat_list)
 
-cost("avg_energy", "Time Average Energy Cost", ppo.avg_rewards_energy_list,
-     random.avg_rewards_energy_list, myopic.avg_rewards_energy_list,
-     fixed_04.avg_rewards_energy_list, fixed_1.avg_rewards_energy_list)  # , alg_dqn.avg_rewards_energy_list)
+# cost("avg_energy", "Time Average Energy Cost", ppo2.avg_rewards_energy_list,
+#      random.avg_rewards_energy_list, myopic.avg_rewards_energy_list,
+#      fixed_04.avg_rewards_energy_list, fixed_1.avg_rewards_energy_list)  # , alg_dqn.avg_rewards_energy_list)
 
 
-area_chart('PPO', ppo.avg_rewards_time_list,
-           ppo.avg_rewards_bak_list, ppo.avg_rewards_bat_list)
-area_chart('Random', random.avg_rewards_time_list,
-           random.avg_rewards_bak_list, random.avg_rewards_bat_list)
-area_chart('Myopic', myopic.avg_rewards_time_list,
-           myopic.avg_rewards_bak_list, myopic.avg_rewards_bat_list)
-area_chart('Fixed 0.4kW', fixed_04.avg_rewards_time_list,
-           fixed_04.avg_rewards_bak_list, fixed_04.avg_rewards_bat_list)
-area_chart('Fixed 1kW', fixed_1.avg_rewards_time_list,
-           fixed_1.avg_rewards_bak_list, fixed_1.avg_rewards_bat_list)
+area_chart('PPO1', ppo1.avg_rewards_time_list,
+           ppo1.avg_rewards_bak_list, ppo1.avg_rewards_bat_list)
+# area_chart('PPO2', ppo2.avg_rewards_time_list,
+#            ppo2.avg_rewards_bak_list, ppo2.avg_rewards_bat_list)
+# area_chart('Random', random.avg_rewards_time_list,
+#            random.avg_rewards_bak_list, random.avg_rewards_bat_list)
+# area_chart('Myopic', myopic.avg_rewards_time_list,
+#            myopic.avg_rewards_bak_list, myopic.avg_rewards_bat_list)
+# area_chart('A2C', a2c.avg_rewards_time_list,
+#            a2c.avg_rewards_bak_list, a2c.avg_rewards_bat_list)
+# area_chart('Fixed 0.4kW', fixed_04.avg_rewards_time_list,
+#            fixed_04.avg_rewards_bak_list, fixed_04.avg_rewards_bat_list)
+# area_chart('Fixed 1kW', fixed_1.avg_rewards_time_list,
+#            fixed_1.avg_rewards_bak_list, fixed_1.avg_rewards_bat_list)
 # area_chart('DQN', dqn.avg_rewards_time_list,
 #            dqn.avg_rewards_bak_list, dqn.avg_rewards_bat_list, 1)
